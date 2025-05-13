6aa1da657ac03f9afa1193f611ef79cd98af1b1b
aisinast <popnastya6@icloud.com>
2025-05-13 17:56:30 +0300

    добавлен класс для очищения консоли

---
+++ TradeSystemWithDataBase/src/main/java/org/example/terminal/OutputController.java
@@ -0,0 +1,22 @@
+package org.example.terminal;
+
+import java.util.Scanner;
+
+public class OutputController {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void clearConsole() {
+        try {
+            new ProcessBuilder("/bin/bash", "-c", "clear").inheritIO().start().waitFor();
+        } catch (Exception E) {
+            System.out.println(E);
+        }
+    }
+
+    private static void waitForEnter() {
+        System.out.println("Нажмите \"Enter\", чтобы продолжить");
+        scanner.nextLine();
+        clearConsole();
+    }
+}
