9426383031c9d16da88ec703a681f7851aca0939
aisinast <popnastya6@icloud.com>
2025-05-15 21:24:26 +0300

    добавлены методы для добавления и увольнения работника, написано меню для этого

---
+++ TradeSystemWithDataBase/src/main/java/org/example/Worker.java
@@ -0,0 +1,157 @@
+package org.example;
+
+import org.example.database.DataBase;
+import org.example.services.Check;
+
+import java.sql.*;
+import java.util.List;
+import java.util.Scanner;
+
+public class Worker {
+    int id;
+    String name;
+    String surname;
+    String phoneNumber;
+    int workPlaceId;
+    String status;
+
+    private static String tableName = "workers";
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void hireWorker() {
+        System.out.print("Введите имя сотрудника: ");
+        String name = scanner.nextLine();
+
+        try {
+            Check.stringNotEmpty(name);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+            return;
+        }
+
+        System.out.print("Введите фамилию сотрудника: ");
+        String surname = scanner.nextLine();
+
+        try {
+            Check.stringNotEmpty(surname);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+            return;
+        }
+
+        System.out.print("Введите номер телефона сотрудника (должен начинаться с \"+7\", 12 символов): ");
+        String number = scanner.nextLine();
+
+        try {
+            Check.phoneNumberIsCorrect(number);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+            return;
+        }
+
+        System.out.print("Куда нанимается сотрудник? (1 - склад, 2 - пункт выдачи заказов)\nВаш выбор: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        String status = null;
+        switch (choice) {
+            case 1:
+                status = "работает на складе";
+
+                System.out.println("Выберите номер склада из списка ниже: ");
+
+                List<String> warehouses = DataBase.columnToList("warehouses", "name");
+                String[] warehousesArray = warehouses.toArray(new String[0]);
+
+                for (int i = 0; i < warehousesArray.length; i++) {
+                    System.out.println(i + 1 + ". " + warehousesArray[i]);
+                }
+
+                System.out.print("Ваш выбор: ");
+                int warehouseNumber = scanner.nextInt();
+                scanner.nextLine();
+
+                String warehouseName = warehousesArray[warehouseNumber - 1];
+                int warehouseId = DataBase.getId("warehouses", "name", warehouseName);
+
+                addWorkerIntoTable(name, surname, number, warehouseId, status);
+
+                break;
+            case 2: status = "работает на пункте выдачи";
+            default:
+                System.out.println("Некорректный ввод! Повторите попытку");
+                break;
+        }
+    }
+
+    public static void dismissWorker() {
+        System.out.print("Введите имя сотрудника: ");
+        String name = scanner.nextLine();
+
+        try {
+            Check.stringNotEmpty(name);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+            return;
+        }
+
+        System.out.print("Введите фамилию сотрудника: ");
+        String surname = scanner.nextLine();
+
+        try {
+            Check.stringNotEmpty(surname);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+            return;
+        }
+
+        int id = (int) DataBase.getCellValueByTwoConditions("workers", "id",
+                "name", name, "surname", surname);
+
+        String sqlQuery = "UPDATE workers SET status = 'уволен' WHERE id = ?";
+
+        try (Connection connection = DriverManager.getConnection(DataBase.getDatabaseUrl())) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+            System.out.println("Работник " + name + " " + surname + " уволен");
+        } catch (SQLException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+    }
+
+    private static void addWorkerIntoTable(String name, String surname, String phoneNumber,
+                                           int workPlaceId, String status) {
+        String sqlQuery = "INSERT INTO workers (name, surname, phone_number, work_place_id, " +
+                "status) VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection conn = DriverManager.getConnection(DataBase.getDatabaseUrl())){
+
+            PreparedStatement pstm = conn.prepareStatement(sqlQuery);
+
+            pstm.setString(1, name);
+            pstm.setString(2, surname);
+            pstm.setString(3, phoneNumber);
+            pstm.setInt(4, workPlaceId);
+            pstm.setString(5, status);
+
+            pstm.executeUpdate();
+
+            int id = 0;
+            try (Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery("SELECT last_insert_rowid()")) {
+                if (rs.next()) {
+                    id = rs.getInt(1);
+                }
+            }
+
+            System.out.println("Работник " + name + " " + surname + " успешно добавлен! ID: " + id);
+
+        } catch (SQLException e) {
+            System.err.println("Ошибка при добавлении работника: " + e.getMessage());
+        }
+    }
+}
---
+++ TradeSystemWithDataBase/src/main/java/org/example/menu/WorkerMenu.java
@@ -0,0 +1,55 @@
+package org.example.menu;
+
+import org.example.Worker;
+import org.example.terminal.OutputController;
+import org.example.terminal.Printer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class WorkerMenu {
+    static Scanner scanner = new Scanner(System.in);
+    static Printer printer = new Printer();
+
+    public static void showWorkerMenu() throws FileNotFoundException {
+        OutputController.clearConsole();
+
+        printer.printTextFile(new File("/Users/mac/code/Java/ИиП/second-project-aisinast/" +
+                "TradeSystemWithDataBase/src/main/java/org/example/menu/texts/worker_menu.txt"));
+
+        System.out.print("\nВаш выбор: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+            case 1:
+                OutputController.clearConsole();
+
+                Worker.hireWorker();
+
+                OutputController.waitForEnter();
+                showWorkerMenu();
+                break;
+            case 2:
+                OutputController.clearConsole();
+
+                Worker.dismissWorker();
+
+                OutputController.waitForEnter();
+                showWorkerMenu();
+                break;
+            case 3:
+            case 0:
+                OutputController.clearConsole();
+                AppMenu.showAppMenu();
+                break;
+            default:
+                OutputController.clearConsole();
+                System.out.println("Некорректный ввод! Повторите попытку");
+                showWorkerMenu();
+                OutputController.waitForEnter();
+                break;
+        }
+    }
+}
