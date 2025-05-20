c4636c411c291e075fdec0209701f132208ba16f
aisinast <popnastya6@icloud.com>
2025-05-20 23:09:29 +0300

    добавлен метод для добавления покупателя

---
+++ TradeSystemWithDataBase/src/main/java/org/example/Buyer.java
@@ -0,0 +1,87 @@
+package org.example;
+
+import org.example.database.DataBase;
+import org.example.services.Check;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Buyer {
+    int id;
+    String name;
+    String surname;
+    String city;
+    int shoppingCardId;
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void addNewBuyer() {
+        System.out.print("Введите имя пользователя: ");
+        String name = scanner.nextLine();
+
+        try {
+            Check.stringNotEmpty(name);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Имя пользователя введено некорректно: " + e.getMessage());
+            return;
+        }
+
+        System.out.print("Введите фамилию пользователя: ");
+        String surname = scanner.nextLine();
+
+        try {
+            Check.stringNotEmpty(surname);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Фамилия пользователя введена некорректно: " + e.getMessage());
+            return;
+        }
+
+        System.out.print("Введите номер телефона (начиная с \"+7\", 12 символов) : ");
+        String phoneNumber = scanner.nextLine();
+
+        try {
+            Check.phoneNumberIsCorrect(phoneNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Номер телефона введен некорректно: " + e.getMessage());
+        }
+
+        System.out.print("Введите город пользователя: ");
+        String city = scanner.nextLine();
+
+        try {
+            Check.stringNotEmpty(city);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Город пользователя введен некорректно: " + e.getMessage());
+            return;
+        }
+
+        int shoppingCardId = ShoppingCard.createNewShoppingCard();
+
+        addBuyerIntoTable(name, surname, phoneNumber, city, shoppingCardId);
+    }
+
+    private static void addBuyerIntoTable(String name, String surname, String phoneNumber,
+                                          String city, int shoppingCardId) {
+        try (Connection connection = DriverManager.getConnection(DataBase.getDatabaseUrl())) {
+            String sqlQuery = "INSERT INTO buyers (name, surname, phone_number, city, shopping_card_id)" +
+                    " VALUES (?, ?, ?, ?, ?)";
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, surname);
+                preparedStatement.setString(3, phoneNumber);
+                preparedStatement.setString(4, city);
+                preparedStatement.setInt(5, shoppingCardId);
+
+                preparedStatement.executeUpdate();
+
+                System.out.println("Пользователь " + surname + " " + name + " успешно добавлен!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Ошибка при добавлении пользователя: " + e.getMessage());
+        }
+    }
+}
---
+++ TradeSystemWithDataBase/src/main/java/org/example/menu/BuyerMenu.java
@@ -0,0 +1,44 @@
+package org.example.menu;
+
+import org.example.Buyer;
+import org.example.terminal.OutputController;
+import org.example.terminal.Printer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BuyerMenu {
+
+    static Scanner scanner = new Scanner(System.in);
+    static Printer printer = new Printer();
+
+    public static void showBuyerMenu() {
+        try {
+            printer.printTextFile(new File("src/main/java/org/example/menu/texts/buyer.txt"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.print("\nВаш выбор: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+            case 1:
+                OutputController.clearConsole();
+
+                Buyer.addNewBuyer();
+
+                OutputController.waitForEnter();
+                showBuyerMenu();
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 0:
+            default:
+        }
+    }
+}
